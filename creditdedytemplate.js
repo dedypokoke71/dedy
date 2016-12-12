4b368efcbe0b2a2b6e06af0184c891ecaa69c082
dedypokoke71 <dedypokoke71@gmail.com>
2016-12-13 01:33:58 +0700

    Update creditdedytemplate.js

--- creditdedytemplate.js
+++ creditdedytemplate.js
@@ -1,9 +1,7 @@
-<script type='text/javascript'>
-//<![CDATA[
-$(document).ready(function () {
-    if ($("#protect,.protect").attr("href") != "http://dedy-setiyawan.blogspot.co.id/") {
-        window.location.href = "http://www.musafus.web.id/";
-    }
+<script>
+$(document).ready(function(){
+ $('.credit .kiri').html('Template By <a href="http://dedy-setiyawan.blogspot.co.id/">Dedy Blog Official</a>');
+setInterval(function(){
+    if (!$('.credit .kiri:visible').length) window.location.href = 'http://www.masyadi.com';}, 3000);
 });
-//]]>
 </script>
