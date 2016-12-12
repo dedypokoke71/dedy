f883561ad5d6738c259a0a8b96136c1d711a51b7
dedypokoke71 <dedypokoke71@gmail.com>
2016-12-13 01:20:38 +0700

    Create creditdedytemplate.js

---
+++ creditdedytemplate.js
@@ -0,0 +1,9 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () {
+    if ($("#protect,.protect").attr("href") != "http://dedy-setiyawan.blogspot.co.id/") {
+        window.location.href = "http://www.musafus.web.id/";
+    }
+});
+//]]>
+</script>
